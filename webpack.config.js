bae9ed3081823996d3c8e95d70d33ee58a73fa8f
amartinsmg <andremindurimg@gmail.com>
2022-09-09 23:36:20 -0300

    Add webpack.config and babel.config

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const CssMinimizerWebpack = require("css-minimizer-webpack-plugin"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+  path = require("path"),
+  TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: "./src/script.js",
+  output: {
+    filename: "assets/bundle.js",
+    path: path.resolve(__dirname, "build"),
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  mode: "production",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "build"),
+    },
+    host: "localhost",
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ejs$/,
+        use: ["html-loader", "template-ejs-loader"],
+      },
+      {
+        test: /\.s(c|a)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".scss", ".css"],
+  }, 
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerWebpack()],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ filename: "index.html" }),
+    new MiniCssExtractPlugin({
+      filename: "assets/bundle.css",
+    }),
+  ],
+};
