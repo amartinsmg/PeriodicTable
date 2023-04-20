c86e5a005ad3d34ff6050f99829e1e1805275193
amartinsmg <andremindurimg@gmail.com>
2023-04-20 17:10:20 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
-const CssMinimizerWebpack = require("css-minimizer-webpack-plugin"),
+/**
+  Configuration file for Webpack bundling tool.
+*/
+
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin"),
   HtmlWebpackPlugin = require("html-webpack-plugin"),
   { htmlWebpackPluginTemplateCustomizer } = require("template-ejs-loader"),
   MiniCssExtractPlugin = require("mini-css-extract-plugin"),
@@ -9,47 +13,95 @@ const CssMinimizerWebpack = require("css-minimizer-webpack-plugin"),
   Elements = JSON.parse(JsonFile);
 
 module.exports = {
-  entry: "./src/script.js",
+  /**
+    The entry point for the application.
+  */
+
+  entry: "./src/main.js",
+
+  /**
+      The output configuration for the bundled files.
+      @property filename - The name of the output file.
+      @property path - The path to the output directory.
+      @property assetModuleFilename - The asset module filename pattern.
+    */
+
   output: {
     filename: "assets/bundle.js",
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "assets/[hash][ext]",
   },
+
+  /**
+      The mode configuration for Webpack.
+    */
+
   mode: "production",
+
+  /**
+      The configuration options for the development server.
+      @property static - The static file serving options.
+      @property static.directory - The directory to serve static files from.
+      @property port - The port number to listen on.
+      @property open - Whether to open the default browser on startup.
+    */
+
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "build"),
+      directory: path.resolve(__dirname, "dist"),
     },
     port: 8080,
     open: true,
   },
+
+  /**
+      The module configuration for Webpack.
+      @property rules - The array of rules for processing files.
+    */
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$/i,
         use: "babel-loader",
         exclude: /node_modules/,
       },
       {
-        test: /\.ejs$/,
+        test: /\.ejs$/i,
         use: ["html-loader", "template-ejs-loader"],
       },
       {
-        test: /\.s(c|a)ss$/,
+        test: /\.s(c|a)ss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
+
+  /**
+    The resolve configuration for Webpack.
+    @property extensions - The list of file extensions to resolve.
+  */
+
   resolve: {
-    extensions: [".js", ".ejs", ".sass", ".css"],
+    extensions: [".ts", ".js", ".ejs", ".css"],
   },
+
+  /**
+    The optimization configuration for Webpack.
+    @property minimizer - The array of plugins used for code optimization.
+  */
+
   optimization: {
-    minimizer: [new TerserPlugin(), new CssMinimizerWebpack()],
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
+
+  /**
+    The plugins configuration for Webpack.
+  */
+
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
