9c6c65472449619aae996543b8013e71c8d443f8
André Martins <andremindurimg@gmail.com>
2026-03-09 23:46:22 -0300

    refactor: Improve element display logic and naming
    - Use camelCase for local variables
    - Refactor update method to accept element directly
    - Update internal property name

--- src/main.js
+++ src/main.js
@@ -3,29 +3,29 @@ import "./sass/main.sass";
 // The main function to handle the display of information about atomic elements.
 
 function main() {
-  const AtomicElementsDivs = document.querySelectorAll(".container-element"),
+  const atomicElementsDivs = document.querySelectorAll(".container-element");
+
   /**
     An object that keeps track of the currently displayed atomic element information and provides a method to update it.
-      @property element - The HTML element that currently displays the atomic element information.
+      @property _element - The HTML element that currently displays the atomic element information.
       @property update - A method that updates the currently displayed atomic element information to the element with the provided ID.
   */
-    CurrentDisplayedElement = {
-      element: document.querySelector("#H-info"),
-      update(newElementId) {
-        const NewElement = document.querySelector(newElementId);
-        this.element.classList.add("non-display");
-        NewElement.classList.remove("non-display");
-        this.element = NewElement;
-      },
-    };
 
-  /**
-    Add a click event listener to each ".container-element" element to update the displayed atomic element information.
-  */
+  const currentDisplayedEl = {
+    _element: document.querySelector("#H-info"),
+    update(newElement) {
+      this._element.classList.add("non-display");
+      newElement.classList.remove("non-display");
+      this._element = newElement;
+    },
+  };
 
-  AtomicElementsDivs.forEach((el) => {
+  // Add a click event listener to each ".container-element" element to update the displayed atomic element information.
+
+  atomicElementsDivs.forEach((el) => {
     el.addEventListener("click", () => {
-      CurrentDisplayedElement.update(`#${el.id}-info`);
+      const infoElement = document.querySelector(`#${el.id}-info`);
+      currentDisplayedEl.update(infoElement);
     });
   });
 }
